07c7d11372ab9ce01e3388c606be19a9bc30303b
André Baltieri <andrebaltieri@hotmail.com>
2014-12-17 16:48:25 -0200

    Started proj

---
+++ gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        bower: {
+            install: {
+                options: {
+                    targetDir: "wwwroot/lib",
+                    layout: "byComponent",
+                    cleanTargetDir: false
+                }
+            }
+        },
+        less: {
+            development: {
+                options: {
+                    paths: ["Assets"],
+                },
+                files: {
+                    "wwwroot/assets/css/site.css": "_libs/less/site.less"
+                }
+            },
+        }
+    });
+
+    // This command registers the default task which will install bower packages into wwwroot/lib
+    grunt.registerTask("default", ["bower:install"]);
+
+    // The following line loads the grunt plugins.
+    // This line needs to be at the end of this this file.
+    grunt.loadNpmTasks("grunt-bower-task");
+
+    grunt.loadNpmTasks("grunt-contrib-less");
+};
